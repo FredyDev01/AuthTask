fa78639dd97be1e07f58130a5f6dc6ddb7ab2086
FredyDev01 <fredy12cbpu@gmail.com>
2023-01-04 14:04:06 -0500

    Mejora-Loader

--- src/app/vrfSesion.js
+++ src/app/vrfSesion.js
@@ -9,23 +9,24 @@ const PhoneUser = document.querySelector('#phoneUser')
 
 
 export const vrfUser = user =>{
-    document.getElementById("Loader").classList.add('d-none')
-    document.body.classList.remove('overflow-hidden')
+    document.getElementById("Loader").classList.add('d-none')    
+    document.body.classList.remove('bodyBefore')    
+    document.body.classList.add('bodyAfter')
     if(user){        
         //Ocultar secciones
-        NavSession.forEach(e => e.style.display = 'none')
-        CloseSession.forEach(a => a.style.display = 'block')        
-        CtnSinSession.style.display = 'none'
-        CtnConSession.style.display = 'block'        
+        NavSession.forEach(e => e.classList.add('d-none'))
+        CloseSession.forEach(a => a.classList.remove('d-none'))                
+        CtnSinSession.classList.add('d-none')
+        CtnConSession.classList.remove('d-none')
         //Rellenar data
         ImgUser.setAttribute('src', user.photoURL ? user.photoURL: './assets/UserAnonimo.png')
         NameUser.textContent = user.displayName ? user.displayName : 'Anonimo'
         EmailUser.textContent = user.email
-        PhoneUser.textContent = user.phoneNumber ? user.phoneNumber : 'Desconocido'             
+        PhoneUser.textContent = user.phoneNumber ? user.phoneNumber : 'Desconocido'
     }else{
-        CloseSession.forEach(a => a.style.display = 'none')
-        NavSession.forEach(e => e.style.display = 'block')
-        CtnConSession.style.display = 'none'
-        CtnSinSession.style.display = 'block'                
+        CloseSession.forEach(a => a.classList.add('d-none'))
+        NavSession.forEach(e => e.classList.remove('d-none'))        
+        CtnConSession.classList.add('d-none')
+        CtnSinSession.classList.remove('d-none')
     }
 }
